7bffd74e9546fecbe74dfb09d5674d5a6bac3256
Abhishek6416 <abhishekkr6416@gmail.com>
2023-02-05 09:37:48 +0530

    objectdestructuring

--- JS-HOIST,CLOSURES,PROMISES,ASYNC,AWAIT/index.js
+++ JS-HOIST,CLOSURES,PROMISES,ASYNC,AWAIT/index.js
@@ -21,12 +21,27 @@
 
 // hoisting
 // we havent define  or assign any value to itany variable 
+// console.log(name)
+// var name;
+// name="abhishekh";
+
+
+// object destructuring
+
+const myBiodata={
+    "name":"abhishekh",
+    "age":23,
+    "school":"masai",
+    "likes":"yoyo"
+}
+
+
+let{name,age,school,likes}=myBiodata;
+
 console.log(name)
-var name;
-name="abhishekh";
-
-
-
+console.log(age)
+console.log(school)
+console.log(likes)
 
 
 
