47445dfd81e213cb172f3d2bdbd99ba5fed38fe8
Abhishek6416 <abhishekkr6416@gmail.com>
2023-02-05 09:22:25 +0530

    hoist,closures

---
+++ JS-HOIST,CLOSURES,PROMISES,ASYNC,AWAIT/index.js
@@ -0,0 +1,46 @@
+
+
+// closures
+// const outerFun=(a)=>{
+
+//     let b=10
+
+//     const innerFun=()=>{
+//         let c=6;
+
+//         console.log(`sum is ${a+b+c}`)
+//     }
+
+//     innerFun()
+//     // console.log(`sum is ${a+b+}`)
+// }
+// outerFun(5)
+
+
+
+
+// hoisting
+// we havent define  or assign any value to itany variable 
+console.log(name)
+var name;
+name="abhishekh";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
